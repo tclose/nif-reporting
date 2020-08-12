1961ce008cc03d4e9db014a519526da28c1eb594
Thomas G. Close <tom.g.close@gmail.com>
2020-08-12 15:36:09 +1000

    fixed up DOI access

--- scripts/find_authors.py
+++ scripts/find_authors.py
@@ -1,5 +1,7 @@
+import sys
 from urllib.parse import unquote as unquote_url
 import requests
+from requests.exceptions import ConnectionError
 from bs4 import BeautifulSoup
 import pybliometrics.scopus as sc
 
@@ -8,17 +10,24 @@ SCIENCE_DIRECT = 'http://api.elsevier.com/content/article/pii/'
 
 
 def text_from_doi(doi):
-    html = BeautifulSoup(requests.get(DOI_RESOLVER + doi).text)
-    redirect_url = unquote_url(html.find(id='redirectURL').attrs['value'])
-    return requests.get(redirect_url).text
-
+    try:
+        response = requests.get(DOI_RESOLVER + doi)
+    except ConnectionError:
+        return None
+    html = BeautifulSoup(response.text, features='lxml')
+    redirect_tag = html.find(id='redirectURL')
+    if redirect_tag:
+        redirect_url = unquote_url(redirect_tag.attrs['value'])
+        html = BeautifulSoup(requests.get(redirect_url).text,
+                             features='lxml')
+    return html
 
 def text_from_pii(pii):
     response = requests.get(
         SCIENCE_DIRECT + pii,
         headers={"X-ELS-APIKey"  : sc.config['Authentication']['APIKey'],
                  "Accept"        : 'application/json'})
-    return response['full-text-retrieval-response']
+    return response.json()['full-text-retrieval-response']
 
 
 AUTHORS = [
@@ -83,7 +92,9 @@ for pub in publications:
         full_text = text_from_pii(pub.pii)
     elif pub.doi:
         full_text = text_from_doi(pub.doi)
+        if full_text is None:
+            full_text = "Could not access DOI ({})".format(pub.doi)
     else:
-        full_text "Could not find DOI or PII for title!!!"
+        full_text = "Could not find DOI or PII for title!!!"
         
-    print(full_text + '\n\n=============================================\n\n')
+    print(str(full_text) + '\n\n=============================================\n\n')
