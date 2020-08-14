a704605b75d94efa93d7f023d995308307d8d2f7
Tom Close <tom.g.close@gmail.com>
2020-08-14 17:15:49 +1000

    finished script to export publications to NRT format

--- scripts/export_csv.py
+++ scripts/export_csv.py
@@ -1,12 +1,9 @@
 """
 Search through publication content to search for likely terms
 """
-import os
-import re
 import csv
-import logging
 from argparse import ArgumentParser
-from app import app, db
+from app import app
 from app.models import Publication
 from app.constants import DEFINITE_NIF_ASSOC
 
@@ -19,9 +16,9 @@ CSV_HEADERS = [
     'IP or Commercialisation Activity', 'Granting Body',
     'Link for further info', 'Duration (years)', 'Funding Amount',
     'Announcement Link', 'File Number', 'Link', 'Audience',
-    'Outcomes (including potential)', 'Status', 'Asset Type', '"Asset Type',
-    ' if Other"', 'Data Asset Link', 'Is NIF Acknowledged?', '"If not',
-    ' why"', 'Publication', 'Link to Software', 'Access Assigned',
+    'Outcomes (including potential)', 'Status', 'Asset Type',
+    '"Asset Type, if Other"', 'Data Asset Link', 'Is NIF Acknowledged?',
+    '"If not, why"', 'Publication', 'Link to Software', 'Access Assigned',
     'Software Description', 'Advice Origin', '"Output type', ' if Other"',
     'Has Associated Project']
 
@@ -31,55 +28,26 @@ args = parser.parse_args()
 
 
 with app.app_context(), open(args.output_csv, 'w') as csv_f:
-    
+
     csv_writer = csv.DictWriter(csv_f, CSV_HEADERS)
 
     csv_writer.writeheader()
 
-    for pub in Publication.query.filter_by(
-            Publication.nif_assoc == DEFINITE_NIF_ASSOC).all():
+    for pub in (Publication.query
+                .filter_by(
+                    nif_assoc=DEFINITE_NIF_ASSOC)
+                .order_by(
+                    Publication.date)).all():
 
         csv_writer.writerow({
             'Subject or Title': pub.title,
-            'Outputs Owner': pub.,
-            'Outputs Owner ID': pub.,
-            'Created Time': pub.,
-            'Modified Time': pub.,
-            'Last Activity Time': pub.,
-            'Tag': pub.,
-            'Fellow Named Author': pub.,
-            'Output Date': pub.,
-            'Origin': pub.,
-            'Output Type': pub.,
-            'Node Contact': pub.,
-            'DOI or Link': pub.,
-            'Advice Provided': pub.,
-            'Government Priority': pub.,
-            'Output summary': pub.,
-            'IP or Commercialisation Activity': pub.,
-            'Granting Body': pub.,
-            'Link for further info': pub.,
-            'Duration (years)': pub.,
-            'Funding Amount': pub.,
-            'Announcement Link': pub.,
-            'File Number': pub.,
-            'Link': pub.,
-            'Audience': pub.,
-            'Outcomes (including potential)': pub.,
-            'Status': pub.,
-            'Asset Type': pub.,
-            '"Asset Type': pub.,
-            ' if Other"': pub.,
-            'Data Asset Link': pub.,
-            'Is NIF Acknowledged?': pub.,
-            '"If not': pub.,
-            ' why"': pub.,
-            'Publication': pub.,
-            'Link to Software': pub.,
-            'Access Assigned': pub.,
-            'Software Description': pub.,
-            'Advice Origin': pub.,
-            '"Output type': pub.,
-            ' if Other"': pub.,
-            'Has Associated Project'
-        })
+            'Fellow Named Author': 'FALSE',
+            'Output Date': pub.date.strftime('%d/%m/%Y'),
+            'Origin': 'Research Community',
+            'Output Type': 'Publication',
+            'Node Contact': 'Prof. Fernando Calamante',
+            'DOI or Link': 'http://dx.doi.org/' + pub.doi,
+            'Is NIF Acknowledged?': 'No',
+            '"If not, why"': 'Offsite instrument',
+            'Publication': pub.pub_name,
+            'Has Associated Project': 'No'})
