d7fce78a0a8a9b3434a9c8d609f2806c1bbd816b
Tom Close <tom.g.close@gmail.com>
2022-08-01 17:57:53 +1000

    added readme and updated add_authors script to use argparse

--- scripts/add_authors.py
+++ scripts/add_authors.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """
-Script to find Scopus records corresponding to list of authors
+Script to add authors to the database of key users (CIs) of the facility
 """
 import os.path
-import click
 from argparse import ArgumentParser
 import pybliometrics.scopus as sc
 from sqlalchemy import orm
@@ -28,8 +27,8 @@ from app.models import Researcher, ScopusAuthor, Affiliation
 #     ('Michael', 'Barnett', None),
 #     ('Simon', 'Lewis', 'S.J.G.')]
 
-AUTHORS = [
-    ('Ramon', 'Landin-Romero', None)]
+# AUTHORS = [
+#     ('Ramon', 'Landin-Romero', None)]
 
 VALID_AREAS = [
     'MEDI',
@@ -38,13 +37,13 @@ VALID_AREAS = [
     'PSYC']
 
 
-@click.command(help="""Add authors to database
-
-AUTHORS a list of authors to add to the database""")
-@
 parser = ArgumentParser(__doc__)
-parser.add_argument('--author', nargs=3, type=str, default=None,
-                    help="The year to search for")
+parser.add_argument('first', type=str,
+                    help="First name of author to add to the database")
+parser.add_argument('last', type=str,
+                    help="Last name of author to add to the database")
+parser.add_argument('--initials', '-i', type=str, default=None,
+                    help="Initials of the author to add to the database")
 args = parser.parse_args()
 
 with app.app_context():
@@ -52,46 +51,47 @@ with app.app_context():
     if not os.path.exists(app.config['SQLALCHEMY_DATABASE_URI']):
         db.create_all()
 
-    for first, last, initials in AUTHORS:
-        all_authors = set(
-            a for a in sc.AuthorSearch("authfirst({}) and authlast({})"
-                                       .format(first, last)).authors
-            if a.givenname.startswith(first))
-        all_authors |= set(
-            a for a in sc.AuthorSearch("authfirst({}.) and authlast({})"
-                                       .format(first[0], last)).authors
-            if a.givenname.startswith(first[0] + '.')
-            or a.givenname.startswith(first))
-        authors = [a for a in all_authors if a.city == 'Sydney']
-        if not authors:
-            authors = [a for a in all_authors if a.country == 'Australia']
-        authors = [
-            a for a in authors
-            if (any(r in a.areas for r in VALID_AREAS) or a.areas == ' ()')
-            and (a.surname.startswith(last) or last not in a.surname)]
-        if initials:
-            authors = [a for a in authors if initials == a.initials]
+    first, last, initials = args.first, args.last, args.initials
 
-        researcher = Researcher(
-            first,
-            last,
-            initials=initials)
-        db.session.add(researcher)
-        for author in authors:
-            try:
-                affiliation = Affiliation.query.filter_by(
-                    scopus_id=author.affiliation_id).one()
-            except orm.exc.NoResultFound:
-                affiliation = Affiliation(
-                    author.affiliation_id, author.affiliation, author.city,
-                    author.country)
-            scopus_author = ScopusAuthor(
-                int(author.eid.split('-')[-1]),
-                givenname=author.givenname,
-                surname=author.surname,
-                researcher=researcher,
-                affiliation=affiliation,
-                areas=author.areas)
-            researcher.scopus_authors.append(scopus_author)
+    all_authors = set(
+        a for a in sc.AuthorSearch("authfirst({}) and authlast({})"
+                                    .format(first, last)).authors
+        if a.givenname.startswith(first))
+    all_authors |= set(
+        a for a in sc.AuthorSearch("authfirst({}.) and authlast({})"
+                                    .format(first[0], last)).authors
+        if a.givenname.startswith(first[0] + '.')
+        or a.givenname.startswith(first))
+    authors = [a for a in all_authors if a.city == 'Sydney']
+    if not authors:
+        authors = [a for a in all_authors if a.country == 'Australia']
+    authors = [
+        a for a in authors
+        if (any(r in a.areas for r in VALID_AREAS) or a.areas == ' ()')
+        and (a.surname.startswith(last) or last not in a.surname)]
+    if initials:
+        authors = [a for a in authors if initials == a.initials]
 
-        db.session.commit()
+    researcher = Researcher(
+        first,
+        last,
+        initials=initials)
+    db.session.add(researcher)
+    for author in authors:
+        try:
+            affiliation = Affiliation.query.filter_by(
+                scopus_id=author.affiliation_id).one()
+        except orm.exc.NoResultFound:
+            affiliation = Affiliation(
+                author.affiliation_id, author.affiliation, author.city,
+                author.country)
+        scopus_author = ScopusAuthor(
+            int(author.eid.split('-')[-1]),
+            givenname=author.givenname,
+            surname=author.surname,
+            researcher=researcher,
+            affiliation=affiliation,
+            areas=author.areas)
+        researcher.scopus_authors.append(scopus_author)
+
+    db.session.commit()
